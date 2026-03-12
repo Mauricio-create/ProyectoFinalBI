f4a584895aa37de9fe5112a3e3b8eb14a67d1fe7
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2026-03-11 23:46:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Modules.choropleth_map import ChoroplethMapGenerator
 from Modules.header import show_header
 from pathlib import Path
 import streamlit as st
+st.set_page_config(layout="wide")
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -12,15 +13,27 @@ CENSO_CSV = BASE_DIR / "Data/AGEB/conjunto_de_datos_ageb_urbana_09_cpv2020.csv"
 AGEB_SHP = BASE_DIR / "Data/shape/AGEB_urb_2010_5.shp"
 CP_SHP = BASE_DIR / "Data/shape_cp/CP_09CDMX_v7.shp"
 
-
-pipeline = GeoDataPipeline(
-    CENSO_CSV,
-   AGEB_SHP,
-    CP_SHP
-)
-
-ageb_cp_fast, cp_geojson = pipeline.run_pipeline()
 st.set_page_config(layout="wide")
+
+@st.cache_data
+def run_pipeline_cached():
+    pipeline = GeoDataPipeline(
+        CENSO_CSV,
+        AGEB_SHP,
+        CP_SHP
+    )
+    return pipeline.run_pipeline()
+
+ageb_cp_fast, cp_geojson = run_pipeline_cached()
+
+#pipeline = GeoDataPipeline(
+#    CENSO_CSV,
+#   AGEB_SHP,
+#    CP_SHP
+#)
+
+#ageb_cp_fast, cp_geojson = pipeline.run_pipeline()
+
 show_header("Mi primera GUI en Streamlit")
 #print(ageb_cp_fast)
 
