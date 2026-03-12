324006acbb7ed742f35436804867267317197c11
Mauricio Castelazo <0236968@up.edu.mx>
2026-03-11 21:25:29 -0600

    images

--- Modules/header.py
+++ Modules/header.py
@@ -1,11 +1,14 @@
 import streamlit as st
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 def show_header(text_title: str):
     # Layout: logo + title side by side
     col1, col2 = st.columns([1, 6])
     
     with col1:
-        st.image("imagenes/UPlogo.jpg", width=200)
+        st.image(f"{BASE_DIR}/imagenes/UPlogo.jpg", width=200)
         
     with col2:
         st.title(text_title)
