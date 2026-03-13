4cf5958e17040282cdd3f6982f86623e2e4ad18c
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2026-03-12 22:37:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,26 @@ st.set_page_config(layout="wide")
 BASE_DIR = Path(__file__).resolve().parent
 
 #---------------------------------------------------------------------------------
-# ... (Tu código comentado del pipeline se mantiene igual)
+#Sección de código donde se hace la carga de datos
+#CENSO_CSV = BASE_DIR / "Data/AGEB/conjunto_de_datos_ageb_urbana_09_cpv2020.csv"
+#AGEB_SHP = BASE_DIR / "Data/shape/AGEB_urb_2010_5.shp"
+#CP_SHP = BASE_DIR / "Data/shape_cp/CP_09CDMX_v7.shp"
+
+#@st.cache_data(show_spinner="Procesando datos geoespaciales...", ttl=None)
+#def run_pipeline_cached(version="1"):
+#    pipeline = GeoDataPipeline(
+#        CENSO_CSV,
+#        AGEB_SHP,
+#        CP_SHP
+#    )
+#    return pipeline.run_pipeline()
+
+#ageb_cp_fast, cp_geojson = run_pipeline_cached("1")
+
+#ageb_cp_fast.to_csv("ageb_cp_fast.csv.gz",index=False)
+
+#with open("cp_geojson.json", "w") as f:
+#    json.dump(cp_geojson, f)
 #-----------------------------------------------------------------------------
 
 AGEB = BASE_DIR / "Data/Processed/ageb_cp_fast.csv.gz"
