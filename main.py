aa5839bb8461a7abe07ba431eed795b0c95163b2
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2026-03-12 20:31:50 -0600

    Data_p

--- main.py
+++ main.py
@@ -4,26 +4,44 @@ from Modules.choropleth_map import ChoroplethMapGenerator
 from Modules.header import show_header
 from pathlib import Path
 import streamlit as st
+import json
+import pandas as pd
 st.set_page_config(layout="wide")
 
 
 BASE_DIR = Path(__file__).resolve().parent
 
-CENSO_CSV = BASE_DIR / "Data/AGEB/conjunto_de_datos_ageb_urbana_09_cpv2020.csv"
-AGEB_SHP = BASE_DIR / "Data/shape/AGEB_urb_2010_5.shp"
-CP_SHP = BASE_DIR / "Data/shape_cp/CP_09CDMX_v7.shp"
+#---------------------------------------------------------------------------------
+#Sección de código donde se hace la carga de datos
+#CENSO_CSV = BASE_DIR / "Data/AGEB/conjunto_de_datos_ageb_urbana_09_cpv2020.csv"
+#AGEB_SHP = BASE_DIR / "Data/shape/AGEB_urb_2010_5.shp"
+#CP_SHP = BASE_DIR / "Data/shape_cp/CP_09CDMX_v7.shp"
 
-@st.cache_data(show_spinner="Procesando datos geoespaciales...", ttl=None)
-def run_pipeline_cached(version="1"):
-    pipeline = GeoDataPipeline(
-        CENSO_CSV,
-        AGEB_SHP,
-        CP_SHP
-    )
-    return pipeline.run_pipeline()
+#@st.cache_data(show_spinner="Procesando datos geoespaciales...", ttl=None)
+#def run_pipeline_cached(version="1"):
+#    pipeline = GeoDataPipeline(
+#        CENSO_CSV,
+#        AGEB_SHP,
+#        CP_SHP
+#    )
+#    return pipeline.run_pipeline()
 
-ageb_cp_fast, cp_geojson = run_pipeline_cached("1")
+#ageb_cp_fast, cp_geojson = run_pipeline_cached("1")
+
+#ageb_cp_fast.to_csv("ageb_cp_fast.csv.gz",index=False)
+
+#with open("cp_geojson.json", "w") as f:
+#    json.dump(cp_geojson, f)
+#-----------------------------------------------------------------------------
+
+
+AGEB = BASE_DIR / "Data/Processed/ageb_cp_fast.csv.gz"
+GEOJSON = BASE_DIR / "Data/Processed/cp_geojson.json"
 show_header("Mi primera GUI en Streamlit")
+
+ageb_cp_fast = pd.read_csv(AGEB)
+with open(GEOJSON) as f:
+    cp_geojson = json.load(f)
 
 
 # gráfico de barras
